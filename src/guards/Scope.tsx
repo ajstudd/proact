41d6f76fc3b48a776719edb93e270a28a90dfa39
ajstudd <j7654894110@gmail.com>
2025-03-11 19:15:43 +0530

    fixed scope

--- src/guards/Scope.tsx
+++ src/guards/Scope.tsx
@@ -1,34 +1,34 @@
-import { UserAuthResponsePayload } from '../types';
-import React, { PropsWithChildren, useEffect, useState } from 'react';
+// import { UserAuthResponsePayload } from '../types';
+// import React, { PropsWithChildren, useEffect, useState } from 'react';
 
-interface ScopeProps {
-  scope: string;
-}
+// interface ScopeProps {
+//   scope: string;
+// }
 
-const Scope = (props: PropsWithChildren<ScopeProps>) => {
-  const { children, scope } = props;
-  const [allow, setAllow] = useState<boolean>(false);
+// const Scope = (props: PropsWithChildren<ScopeProps>) => {
+//   const { children, scope } = props;
+//   const [allow, setAllow] = useState<boolean>(false);
 
-  useEffect(() => {
-    const authData = localStorage.getItem('authData');
-    if (authData) {
-      const data = JSON.parse(authData) as UserAuthResponsePayload;
-      if (data?.user?.role?.scopes) {
-        const scopes = data.user.role.scopes;
-        if (!scopes.includes(scope)) {
-          setAllow(false);
-        } else {
-          setAllow(true);
-        }
-      }
-    }
-  }, []);
+//   useEffect(() => {
+//     const authData = localStorage.getItem('authData');
+//     if (authData) {
+//       const data = JSON.parse(authData) as UserAuthResponsePayload;
+//       if (data?.user?.role?.scopes) {
+//         const scopes = data.user.role.scopes;
+//         if (!scopes.includes(scope)) {
+//           setAllow(false);
+//         } else {
+//           setAllow(true);
+//         }
+//       }
+//     }
+//   }, []);
 
-  if (!allow) {
-    return null;
-  }
+//   if (!allow) {
+//     return null;
+//   }
 
-  return <React.Fragment>{children}</React.Fragment>;
-};
+//   return <React.Fragment>{children}</React.Fragment>;
+// };
 
-export default Scope;
+// export default Scope;
